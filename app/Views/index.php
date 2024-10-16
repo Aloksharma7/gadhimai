a2b1a8f4528a91fc4ec49ccc44a4a3c0e0ab3540
thedhiresh <kushwahadhiresh1@gmail.com>
2024-10-16 10:41:29 +0545

    Added gallery section and government section

--- app/Views/index.php
+++ app/Views/index.php
@@ -80,55 +80,102 @@
 
 <!-- photo gallery -->
 <section class="photo-gallery">
-            <h2>Photo Gallery</h2>
+            <h2 class="heading">Photo Gallery</h2>
+            <img src="/assets/image/divider.svg" class="divider" >
            <!-- Gallery -->
            <table>
 <tr>
 <td  rowspan="2" class="col-md-6">
-<img src="https://via.placeholder.com/600x400" alt="Main Photo">
+<img src="/assets/image/gallery/gadhi%20mai%20mandir.webp" alt="Main Photo">
 </td>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3">
+<img src="/assets/image/gallery/gadhi%20mai%20yag.webp" alt="Smaller Photo 1">
 </td>
 </tr>
 <tr>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3">
+<img src="/assets/image/gallery/gadhi%20mai%20yag2.webp" alt="Smaller Photo 1">
 </td>
 </tr>
 
 </table>
 
 <table>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3">
+<img src="/assets/image/gallery/Gadhimai.webp" alt="Smaller Photo 1">
 </td>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3">
+<img src="/assets/image/gallery/Gadhimai%20temple%20puja.webp" alt="Smaller Photo 1">
 </td>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3">
+<img src="/assets/image/gallery/Gadhimai%20temple.webp" alt="Smaller Photo 1">
 </td>
 </table>
 <table>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3" >
+<img style="height:14rem" src="/assets/image/gallery/Gadhimai.webp" alt="Smaller Photo 1">
 </td>
-<td class="col-md-3">>
-<img src="https://via.placeholder.com/300x200" alt="Smaller Photo 1">
+<td class="col-md-3">
+<img style="height:14rem" src="/assets/image/gallery/statu%20of%20gadhi%20ma.webp" alt="Smaller Photo 1">
 </td>
 
 </table>
 
+
+
+
           
 <!-- Gallery -->
-            <a href="#" class="view-more">View More</a>
-        
+<button class= "btn btn-primary" style="margin-top:7px;">View More 
+
+<svg id="MDI_arrow-left-thin-circle-outline" data-name="MDI / arrow-left-thin-circle-outline" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+<g id="Boundary" fill="#FFAF42" stroke="rgba(0,0,0,0)" stroke-width="1" opacity="0">
+    <rect width="22" height="22" stroke="none"/>
+    <rect x="0.5" y="0.5" width="21" height="21" fill="none"/>
+</g>
+<path id="Path_arrow-left-thin-circle-outline" data-name="Path / arrow-left-thin-circle-outline" d="M18.528,11.167a7.361,7.361,0,1,1-7.361-7.361,7.39,7.39,0,0,1,7.361,7.361m1.806,0a9.167,9.167,0,1,0-9.167,9.167,9.147,9.147,0,0,0,9.167-9.167m-7.755-.917V7.5l3.63,3.667-3.63,3.667v-2.75H6.125V10.25" transform="translate(-0.167 -0.167)" fill="#FFAF42"/>
+</svg>
+</button>        
     </section>
+    <section class="development-partner development-project container-fluid">
+        <h2 class="heading"> Government of Madesh Pradesh - Endowment Department</h2>
+        <img src="/assets/image/divider.svg" class="divider" >
+        <div class="row card-row d-flex justify-content-between development-partner ">
+    <div class="col-md-3 mb-3">
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="col-md-3 mb-3">
+        <div class="card h-100">
+            <img src="/assets/image/political/Upendra-Yadav-new-1-1024x1024.jpg.bv.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 
+    <div class="col-md-3 mb-3">
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
 
+</section>
 
 
 
